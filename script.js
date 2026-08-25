73c74399cdac23a30f189fd59b082985113bd8fd
cashcult26 <nsavaldez99@gmail.com>
2026-08-25 14:42:55 -0600

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("orderForm").addEventListener("submit", function(e){
-  e.preventDefault();
-  document.getElementById("message").textContent =
-  "REQUEST RECEIVED! Before publishing the site, connect this form to your business email, Shopify, Wix, or another order system so customer requests are delivered to you.";
-});
